275b412f459cffca9fd4ae64ecdae8779bd51654
pratikderepatil <pratikdere333@gmail.com>
2023-01-27 14:04:03 +0530

    Register user

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -3,4 +3,11 @@ const UserModel = require("../models/user.model");
 
 const app = express.Router();
 
+app.post("/register", async (req, res) => {
+	let { name, score, difficulty, level } = req.body;
+	let newUser = UserModel({ name, score, difficulty, level });
+	await newUser.save();
+	return res.status(200).send({ message: "User Created", id: newUser.id });
+});
+
 module.exports = app;
